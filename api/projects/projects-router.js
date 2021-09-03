7c756752b866cc9483bfc35b9553a08fc6eece0a
thekid510 <76237067+thekid510@users.noreply.github.com>
2021-09-03 15:43:59 -0700

    wip

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,31 @@
 // Write your "projects" router here!
+const express = require('express')
+const { validateProjectId } = require('./projects-middleware')
+// const {
+
+// } = require('../projects/projects-middleware')
+
+const Projects = require('./projects-model')
+
+const router = express.Router()
+router.get('/',(req, res)=> {
+    Projects.get()
+    .then(projects => {
+        res.json(projects)
+    }) 
+    .catch(err => {
+        res.status(500).json({
+            message:"The Project could not be retrieved",
+            err: err.stack,
+            stack: err.stack,
+        })
+    
+    })
+       
+})
+    router.get('/:id', validateProjectId, (req, res)=> {
+       res.json(req.project)
+    })
+    
+    
+    module.exports = router
--- api/server.js
+++ api/server.js
@@ -1,11 +1,16 @@
 const express = require('express');
+const { logger } = require('./projects/projects-middleware')
+const projectsRouter = require('./projects/projects-router')
 const server = express();
 
 server.use(express.json())
-
+server.use(logger)
 
 // Build your actions router in /api/actions/actions-router.js
 // Build your projects router in /api/projects/projects-router.js
+
+server.use('/api/projects', projectsRouter)
+
 
 server.use('/',(req,res)=> {
     res.send(`<h2>Lets Get Started</h2>`)
