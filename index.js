7cf8f505c38a78ae15c1ed42b1d468da74ff098a
thekid510 <76237067+thekid510@users.noreply.github.com>
2021-09-03 11:38:47 -0700

    wip

--- index.js
+++ index.js
@@ -12,3 +12,15 @@ I need this code, but don't know where, perhaps should make some middleware, don
 
 Pull your server into this file and start it!
 */
+require('dotenv').config()
+
+const { PORT } = require('./config')
+const express = require('express')
+
+const server = express()
+
+server.use(express.json())
+
+server.listen(PORT || 5000,()=> {
+    console.log(`Server listening on`, PORT || 5000)
+})
